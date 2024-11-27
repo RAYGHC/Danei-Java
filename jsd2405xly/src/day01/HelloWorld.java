7bc48068501c6e364bc7bf6b114ec9f252d4d5b4
Ray <Ray@gmail.com>
2024-11-28 03:14:22 +0800

    initial files

---
+++ jsd2405xly/src/day01/HelloWorld.java
@@ -0,0 +1,17 @@
+package day01; //claim package day01(楼+单元)
+
+public class HelloWorld {
+    // main method, 程序的入口
+    public static void main(String[] args) {//运行程序时系统自动执行
+        //输出hello world
+        //1)严格区分大小写 case-sensitive
+        //2)所有符号必须是英文模式
+        //3)每句话必须以分号结尾
+        //4)command + D 快速复制粘贴一整行
+        System.out.println("Hello world");//双引号中的原样输出
+        System.out.println("Hello world1");
+        System.out.println("Hello world2");
+        System.out.println("Hello world3");
+        System.out.println("Hello world4");
+    }
+}
---
+++ jsd2405xly/src/day01/VarDemo.java
@@ -0,0 +1,26 @@
+package day01;
+//variable demonstration
+//单行
+/*
+多行
+ */
+/**
+ * 文档
+ */
+public class VarDemo {
+
+    public static void main(String[] args) {
+        /*
+        变量的语法：
+        数据类型 变量名 = 数据;
+        int a = 250;
+        变量的注意事项：
+        1）变量的值可以修改
+        2)对变量的使用，就是对它所存那个数的使用
+         */
+        int a = 250; //声明一个整型的变量a,并赋值250(assignment)
+        int b = 5, c = 10, d = 100;//声明3个整型变量b,c,d,并赋值5，10，100
+        b = 55;
+        int e = c + 50;//声明整型变量 并赋值为c+50
+    }
+}
